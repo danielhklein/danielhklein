9ce0ae2043cdec274b44252be31b86c16174223a
Daniel H Klein <danielklein@utexas.edu>
2018-07-11 11:01:05 -0700

    full lambda function

--- site-upload-lambda.py
+++ site-upload-lambda.py
@@ -3,15 +3,18 @@ import zipfile
 from io import BytesIO
 from botocore.client import Config
 
-s3 = boto3.resource('s3', config=Config(signature_version='s3v4'))
-site_bucket = s3.Bucket('www.danielhklein.com')
-build_bucket = s3.Bucket('build.danielhklein.com')
-
-site_zip = BytesIO()
-build_bucket.download_fileobj('build.zip', site_zip)
-
-with zipfile.ZipFile(site_zip) as myzip:
-    for nm in myzip.namelist():
-        obj = myzip.open(nm)
-        site_bucket.upload_fileobj(obj, nm)
-        site_bucket.Object(nm).Acl().put(ACL='public-read')
+def lambda_handler(event, context):
+    s3 = boto3.resource('s3', config=Config(signature_version='s3v4'))
+    site_bucket = s3.Bucket('www.danielhklein.com')
+    build_bucket = s3.Bucket('build.danielhklein.com')
+    
+    site_zip = BytesIO()
+    build_bucket.download_fileobj('build.zip', site_zip)
+    
+    with zipfile.ZipFile(site_zip) as myzip:
+        for nm in myzip.namelist():
+            obj = myzip.open(nm)
+            site_bucket.upload_fileobj(obj, nm)
+            site_bucket.Object(nm).Acl().put(ACL='public-read')
+    
+    return
